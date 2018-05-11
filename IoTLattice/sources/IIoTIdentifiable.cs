7426f06d0af6139e5b39d442f4bb57eb35c71f8d
Xavier Schott <assembla@thegothicparty.com>
2018-05-11 10:37:07 -0700

    Migrate to NetStandard 1.3

---
+++ IoTLattice/sources/IIoTIdentifiable.cs
@@ -0,0 +1,19 @@
+﻿namespace IoTLattice
+{
+    /// <summary>
+    /// IoT (Internet of things) transmitor: something that emits a signal
+    /// </summary>
+    public interface IIoTIdentifiable
+    {
+
+        /// <summary>
+        /// The Media Access Control address (Unique identifier)
+        /// </summary>
+        ulong MACAddress();
+
+        /// <summary>
+        /// The Received Signal Strength Indication (in dBm)
+        /// </summary>
+        double RSSI();
+    }
+}
---
+++ IoTLattice/sources/IoTRssiHistory.cs
@@ -0,0 +1,11 @@
+﻿namespace IoTLattice
+{
+    /// <summary>
+    /// A structure combining an IIoTIdentifiable and a RssiHistory
+    /// </summary>
+    public struct IoTRssiHistory
+    {
+        public IIoTIdentifiable ioTIdentifiable;
+        public RssiHistory rssiHistory;
+    }
+}
